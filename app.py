c30745efc444ca0bf617f5eb5c1627ab48481389
Metal Omega <metalomega@gmail.com>
2023-04-24 23:33:07 -0300

    Teste upload

---
+++ app.py
@@ -0,0 +1,10 @@
+'''O sistema de login deve permitir que novos usuários sejam cadastrados, e que usuários existentes possam fazer o login. O sistema deve alertar caso a senha e login não estejam corretos.'''
+
+# O sistema de login deve permitir que novos usuários sejam cadastrados
+
+# O sistema não deve permitir que usuários duplicados sejam cadastrados
+
+# O sistema deve permitir que usuários existentes possam fazer o login
+
+# O sistema deve alertar caso a senha e login não estejam corretos
+ 
